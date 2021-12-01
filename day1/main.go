ff7cd1adc95bbf9c3078c00da0d7e7b8c606a06a
post <61803796+post04@users.noreply.github.com>
2021-12-01 00:53:49 -0500

    Update main.go

--- day1/main.go
+++ day1/main.go
@@ -37,19 +37,14 @@ func main() {
 	fmt.Println("part one:", more)
 	more = 0
 	base = 0
-	//fmt.Println(len(nums1))
 	for i := 0; i < len(nums1); i++ {
 		if i > 1 {
 			if base == 0 {
 				base = nums1[i-1] + nums1[i-2] + nums1[i]
-				//fmt.Println(nums1[i-1]+nums1[i-2]+nums1[i], "(no-change)", nums1[i-2], nums1[i-1], nums1[i])
 				continue
 			}
 			if base < nums1[i-1]+nums1[i-2]+nums1[i] {
-				//fmt.Println(nums1[i-1]+nums1[i-2]+nums1[i], "(increased)", nums1[i-2], nums1[i-1], nums1[i])
 				more++
-			} else {
-				//fmt.Println(nums1[i-1]+nums1[i-2]+nums1[i], "(decreased)", nums1[i-2], nums1[i-1], nums1[i])
 			}
 			base = nums1[i-1] + nums1[i-2] + nums1[i]
 		}
